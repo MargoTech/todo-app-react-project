8d5dbdd7fb0f05ab7d15232047174c404e0953fc
MargoTech <margobartashevich@gmail.com>
2025-02-01 23:26:22 +0100

    Adding tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
   return (
     <div>
       <h1>ToDo List</h1>
+      <ul>
+        {tasks.map((tasks) => (
+          <li key={tasks.id}>{tasks.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
