090841259b66530f8066209cc4c46825ccd0d1a9
MargoTech <margobartashevich@gmail.com>
2025-02-01 23:38:57 +0100

    Create input fiels and button

--- src/App.js
+++ src/App.js
@@ -2,9 +2,26 @@ import React, { useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const addTask = () => {
+    if (inputValue.trim() !== "") {
+      const newTask = { id: Date.now(), text: inputValue };
+      setTasks([...tasks, newTask]);
+      setInputValue("");
+    }
+  };
+
   return (
     <div>
       <h1>ToDo List</h1>
+      <input>
+        type="text" value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="enter the task"
+      </input>
+      <button onClick={addTask}>Add</button>
+
       <ul>
         {tasks.map((tasks) => (
           <li key={tasks.id}>{tasks.text}</li>
